e2bde71fedf21234d710af0b573fd72673fd4bcc
S.Rasp <s.rasp@lmu.de>
2018-10-24 11:48:03 +0200

    New download scripts

--- image_download.py
+++ image_download.py
@@ -1,39 +1,43 @@
+"""
+Script to download images from NASA Worldview
+"""
 import urllib.request
 import datetime
 from calendar import monthrange
-#Worldview NASA: every day
-#time (December 2007 - February 2017)
-years = range(2007,2018)
-#years = [2008, 2009]
-months = [1,2,12]
-#months = [12]
+import fire
+import os
 
-PATH = '/local/S.Rasp/MPI_Aqua_v2/'
 
-lon1 = -60; lon2 = -20
-lat1 = 5; lat2 = 25
-dlon = lon2 - lon1
-dlat = lat2 - lat1
-deg2pix = 1140/20
-loc= (f'&extent={lon1},{lat1},{lon2},{lat2}')
-size = (f'&width={int(dlon * deg2pix)}&height={int(dlat * deg2pix)}')
-layer = ('&layers=MODIS_Aqua_CorrectedReflectance_TrueColor,Coastlines')
+def download_imgs(year_range, months, save_path, lon_range, lat_range,
+                  deg2pix=100, satellite='Aqua'):
+    os.makedirs(save_path, exist_ok=True)
 
-idate = datetime.datetime(2007,12,1)
-for yr in years:
-  for m in months:
-    if yr == years[0] and m == 1:
-      False
-    elif yr == years[0] and m == 2:
-      False
-    elif yr == years[-1] and m == 12:
-      False
-    else: 
-      nday = monthrange(yr,m)[1]
-      #nday = 2
-      for nd in range(1,nday+1):
-        date = datetime.datetime(yr, m, nd)
-        d = str(date.strftime('%j'))
-        print(date.strftime('%y %m %d'))
-        url = 'https://gibs.earthdata.nasa.gov/image-download?TIME='+str(yr)+d+loc+'&epsg=4326'+layer+'&opacities=1,1&worldfile=false&format=image/png'+size
-        urllib.request.urlretrieve(url,PATH+'Aqua_CorrectedReflectance'+str(yr)+date.strftime('%m')+'{:02d}'.format(date.day)+'.png')
+    lon1 = lon_range[0]; lon2 = lon_range[1]
+    lat1 = lat_range[0]; lat2 = lat_range[1]
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    loc= (f'&extent={lon1},{lat1},{lon2},{lat2}')
+    loc_str = f'_{lon1}-{lon2}_{lat1}-{lat2}'
+    size = (f'&width={int(dlon * deg2pix)}&height={int(dlat * deg2pix)}')
+    layer = (f'&layers=MODIS_{satellite}_CorrectedReflectance_TrueColor,Coastlines')
+
+    for yr in range(year_range[0], year_range[1]):
+        for m in months:
+            nday = monthrange(yr,m)[1]
+            for nd in range(1,nday+1):
+                date = datetime.datetime(yr, m, nd)
+                d = str(date.strftime('%j'))
+                print(date.strftime('%y %m %d'))
+                url = ('https://gibs.earthdata.nasa.gov/image-download?TIME='+
+                       str(yr)+d+loc+'&epsg=4326'+layer+
+                       '&opacities=1,1&worldfile=false&format=image/jpeg'+
+                       size)
+                urllib.request.urlretrieve(
+                    url,
+                    save_path+f'/{satellite}_CorrectedReflectance'+str(yr)+
+                    date.strftime('%m')+'{:02d}'.format(date.day)+loc_str+
+                    '.jpeg')
+
+
+if __name__ == '__main__':
+    fire.Fire(download_imgs)
