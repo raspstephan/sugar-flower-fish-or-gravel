9c8edfc6c4ff33e4f6658def5aead2bb28b119a3
S.Rasp <s.rasp@lmu.de>
2019-01-15 09:53:55 +0100

    Recovered old image download scripts

---
+++ image_download/download_all.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#    1. -61deg E; 10deg N : -40deg E; 24deg N (for season DJF and MAM
+python image_download.py --year_range [2007,2018] --months [12,1,2] --lon_range [-61,-40] --lat_range [10,24] --save_path /project/meteo/work/S.Rasp/cloud-classification2/Region1_DJF_Terra --satellite Terra &&
+python image_download.py --year_range [2007,2018] --months [3,4,5] --lon_range [-61,-40] --lat_range [10,24] --save_path /project/meteo/work/S.Rasp/cloud-classification2/Region1_MAM_Terra --satellite Terra &
+python image_download.py --year_range [2007,2018] --months [12,1,2] --lon_range [-61,-40] --lat_range [10,24] --save_path /project/meteo/work/S.Rasp/cloud-classification2/Region1_DJF_Aqua --satellite Aqua &&
+python image_download.py --year_range [2007,2018] --months [3,4,5] --lon_range [-61,-40] --lat_range [10,24] --save_path /project/meteo/work/S.Rasp/cloud-classification2/Region1_MAM_Aqua --satellite Aqua &
+#    2. 159deg E; 8deg N : 180deg E; 22deg N (for season DJF)
+python image_download.py --year_range [2007,2018] --months [12,1,2] --lon_range [159,180] --lat_range [8,22] --save_path /project/meteo/work/S.Rasp/cloud-classification2/Region2_DJF_Terra --satellite Terra&&
+python image_download.py --year_range [2007,2018] --months [12,1,2] --lon_range [159,180] --lat_range [8,22] --save_path /project/meteo/work/S.Rasp/cloud-classification2/Region2_DJF_Aqua --satellite Aqua&
+#    3. -135deg E; -15deg N : -114deg E; -1deg N (for season DJF and SON)
+python image_download.py --year_range [2007,2018] --months [12,1,2] --lon_range [-135,-114] --lat_range [-15,-1] --save_path /project/meteo/work/S.Rasp/cloud-classification2/Region3_DJF_Terra --satellite Terra&&
+python image_download.py --year_range [2007,2018] --months [9,10,11] --lon_range [-135,-114] --lat_range [-15,-1] --save_path /project/meteo/work/S.Rasp/cloud-classification2/Region3_SON_Terra --satellite Terra&
+python image_download.py --year_range [2007,2018] --months [12,1,2] --lon_range [-135,-114] --lat_range [-15,-1] --save_path /project/meteo/work/S.Rasp/cloud-classification2/Region3_DJF_Aqua --satellite Aqua&&
+python image_download.py --year_range [2007,2018] --months [9,10,11] --lon_range [-135,-114] --lat_range [-15,-1] --save_path /project/meteo/work/S.Rasp/cloud-classification2/Region3_SON_Aqua --satellite Aqua&
+
--- image_download/image_download.py
+++ image_download/image_download.py
@@ -44,5 +44,41 @@ def download_MODIS_imgs(year_range, months, save_path, lon_range, lat_range,
                         print(f'Download failed for {save_str}')
 
 
+def download_imgs(year_range, months, save_path, lon_range, lat_range,
+                  deg2pix=100, satellite='Aqua', exist_skip=False):
+    os.makedirs(save_path, exist_ok=True)
+
+    lon1 = lon_range[0]; lon2 = lon_range[1]
+    lat1 = lat_range[0]; lat2 = lat_range[1]
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    loc= (f'&extent={lon1},{lat1},{lon2},{lat2}')
+    loc_str = f'_{lon1}-{lon2}_{lat1}-{lat2}'
+    size = (f'&width={int(dlon * deg2pix)}&height={int(dlat * deg2pix)}')
+    layer = (f'&layers=MODIS_{satellite}_CorrectedReflectance_TrueColor,Coastlines')
+
+    for yr in range(year_range[0], year_range[1]):
+        for m in months:
+            nday = monthrange(yr,m)[1]
+            for nd in range(1,nday+1):
+                date = datetime.datetime(yr, m, nd)
+                d = str(date.strftime('%j'))
+                print(date.strftime('%y %m %d'))
+                url = ('https://gibs.earthdata.nasa.gov/image-download?TIME='+
+                       str(yr)+d+loc+'&epsg=4326'+layer+
+                       '&opacities=1,1&worldfile=false&format=image/jpeg'+
+                       size)
+                save_str = (save_path+f'/{satellite}_CorrectedReflectance'+str(yr)+
+                    date.strftime('%m')+'{:02d}'.format(date.day)+loc_str+
+                    '.jpeg')
+                if exist_skip and os.path.exists(save_str):
+                    print('Skip')
+                else:
+                    try:
+                        urllib.request.urlretrieve(url, save_str)
+                    except:
+                        print(f'Download failed for {save_str}')
+
+
 if __name__ == '__main__':
     fire.Fire(download_imgs)
